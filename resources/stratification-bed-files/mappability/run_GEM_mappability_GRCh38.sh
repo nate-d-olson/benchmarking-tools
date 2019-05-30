2813be1f9cb8c5f91938d9a7a9cba8250a2ce629
Nate Olson <nolson@nist.gov>
2019-05-30 02:52:37 -0700

    parameterized script

--- resources/stratification-bed-files/mappability/run_GEM_mappability_GRCh38.sh
+++ resources/stratification-bed-files/mappability/run_GEM_mappability_GRCh38.sh
@@ -2,46 +2,57 @@
 #SBATCH -n 8 # number of cores
 #SBATCH --mem 24g # memory pool for all cores
 #SBATCH --job-name=GEM
-#SBATCH --time=10:00:00
+#SBATCH --time=15:00:00
 #SBATCH --partition=msalit,normal
 #SBATCH --mail-type=ALL
  
 echo "Running job $JOB_NAME, $JOB_ID on $HOSTNAME"
 
-
+## Variables defined with job submission
+# l - length
+# m - mismatches
+# e - edit distance
 
 START_STIME=`date +%Y%m%dT%H%M%S`
 START_TIME=`date +%s`
 
 ## Dependencies
-GEM=/oak/stanford/groups/msalit/ndolson/GEM-binaries-Linux-x86_64-core_i3-20130406-045632/bin
+export PATH=$PATH:/oak/stanford/groups/msalit/ndolson/GEM-binaries-Linux-x86_64-core_i3-20130406-045632/bin
 BEDOPS=/oak/stanford/groups/msalit/ndolson/bedops/bin
 
 ## Variables
-REF=/oak/stanford/groups/msalit/shared/genomes/hg38/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna.gz
+REF=/oak/stanford/groups/msalit/shared/genomes/hg38/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna
 REFID=GRCh38_no_alt
 WKDIR=/oak/stanford/groups/msalit/ndolson/GRCh38_mappability/
 
-${GEM}/gem-indexer -i ${REF} -o ${WKDIR}/${REFID}_gemidx --complement emulate -T 8 
 
-${GEM}/gem-mappability -m 2 -e 1 -T 4 -I ${WKDIR}/${REFID}_gemidx.gem -l 250 -o ${WKDIR}/${REFID}_gemmap_l250_m2_e1
+## Indexing reference if index is not present
+IDXFILE=${WKDIR}/${REFID}_gemidx
+if [ -f "${IDXFILE}.gem" ]; then
+    echo "${IDXFILE}.gem exists"
+else 
+	echo "Reference index does not exist, indexing."
+	gem-indexer -i ${REF} -o ${IDXFILE} --complement emulate -T 8 
+fi
 
-${GEM}/gem-2-wig -I ${WKDIR}/${REFID}_gemidx.gem \
-	-i ${WKDIR}/${REFID}_gemmap_l250_m2_e1.mappability \
-	-o ${WKDIR}/${REFID}_gemmap_l250_m2_e1
+gem-mappability -m ${m} -e ${e} -T 8 -I ${IDXFILE}.gem -l ${l} -o ${WKDIR}/${REFID}_gemmap_l${l}_m${m}_e${e}
 
-sed 's/ dna//' ${WKDIR}/${REFID}_gemmap_l250_m2_e1.wig > ${WKDIR}/${REFID}_gemmap_l250_m2_e1_nodna.wig
-sed 's/ dna//' ${WKDIR/${REFID}_gemmap_l250_m2_e1.sizes > ${WKDIR}/${REFID}_gemmap_l250_m2_e1_nodna.sizes
+gem-2-wig -I ${WKDIR}/${REFID}_gemidx.gem \
+	-i ${WKDIR}/${REFID}_gemmap__l${l}_m${m}_e${e}.mappability \
+	-o ${WKDIR}/${REFID}_gemmap__l${l}_m${m}_e${e}
 
-${BEDOPS}/wig2bed -m 16G < ${WKDIR}/${REFID}_v37_gemmap_l250_m2_e1_nodna.wig > ${WKDIR}/${REFID}_gemmap_l250_m2_e1.bed
+sed 's/ dna//' ${WKDIR}/${REFID}_gemmap__l${l}_m${m}_e${e}.wig > ${WKDIR}/${REFID}_gemmap__l${l}_m${m}_e${e}_nodna.wig
+sed 's/ dna//' ${WKDIR/${REFID}_gemmap__l${l}_m${m}_e${e}.sizes > ${WKDIR}/${REFID}_gemmap__l${l}_m${m}_e${e}_nodna.sizes
+
+${BEDOPS}/wig2bed -m 16G < ${WKDIR}/${REFID}_v37_gemmap__l${l}_m${m}_e${e}_nodna.wig > ${WKDIR}/${REFID}_gemmap__l${l}_m${m}_e${e}.bed
 
 awk '$5>0.9' ${WKDIR}/${REFID}_gemmap_l250_m2_e1.bed > ${WKDIR}/${REFID}_gemmap_l250_m2_e1_uniq.bed
 
 
 ## Not sure this is necessary
-#/home/justin.zook/GEM_mappability/wigToBigWig ${WKDIR}/${REFID}_gemmap_l250_m2_e1_nodna.wig \
-#	${WKDIR}/${REFID}_gemmap_l250_m2_e1_nodna.sizes \
-#	${WKDIR}/${REFID}_gemmap_l250_m2_e1.bw
+#/home/justin.zook/GEM_mappability/wigToBigWig ${WKDIR}/${REFID}_gemmap__l${l}_m${m}_e${e}_nodna.wig \
+#	${WKDIR}/${REFID}_gemmap__l${l}_m${m}_e${e}_nodna.sizes \
+#	${WKDIR}/${REFID}_gemmap__l${l}_m${m}_e${e}.bw
 
 END_TIME=`date +%s`
 ELAPSED_TIME=`expr $END_TIME - $START_TIME`
